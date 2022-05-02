fb3ddb373cb0b6fe728d5aa98857d931391b7b89
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2022-05-02 22:16:04 +0200

    SqlContext on IdentityDbContext Migration for database with IdentityUser RegisterUserModel UserController with RegisterUser

---
+++ KenKata.Shared/Models/RegisterUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KenKata.Shared.Models
+{
+    public class RegisterUserModel
+    {
+        [Key]
+        [Required]
+        [EmailAddress(ErrorMessage = "Email must be valid")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password), StringLength(32, ErrorMessage = "Must be at least 8 characters.")]
+        [RegularExpression(@"^((?=.*\d)(?=.*[A-Z]).{8,50})",
+            ErrorMessage =
+                "Must be at least 8 characters, 1 uppercase letter, 1 special character, alphanumeric character")]
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
