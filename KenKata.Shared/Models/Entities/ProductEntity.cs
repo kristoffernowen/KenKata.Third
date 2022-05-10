29401cefb2d7383e800f6a05b0283c5ff7433ed5
jessica <jessicamolin@hotmail.com>
2022-05-10 10:28:17 +0200

    ProductController

---
+++ KenKata.Shared/Models/Entities/ProductEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenKata.Shared.Models.Entities
+{
+    public class ProductEntity
+    {
+    }
+}
---
+++ KenKata.WebApp/Controllers/CostumerProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KenKata.WebApp.Controllers
+{
+    public class CostumerProductController : Controller
+    {
+        public async Task<IActionResult> GetAll()
+        {
+
+            return View();
+        }
+    }
+}
