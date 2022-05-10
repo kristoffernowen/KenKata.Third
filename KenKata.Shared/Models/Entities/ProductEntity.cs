6697ac8a19c24b56def76ede204448b8343bd0f2
jessica <jessicamolin@hotmail.com>
2022-05-10 10:30:33 +0200

    ProductsEntity add to sqlContext

--- KenKata.Shared/Models/Entities/ProductEntity.cs
+++ KenKata.Shared/Models/Entities/ProductEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace KenKata.Shared.Models.Entities
 {
     public class ProductEntity
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(200)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(max)")]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
     }
 }
