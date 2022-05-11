6967c13f35bfb88e41152cfa79265eb5c00b9080
jessica <jessicamolin@hotmail.com>
2022-05-11 19:03:42 +0200

    test

---
+++ KenKata.Shared/Models/ProductModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenKata.Shared.Models
+{
+    public class ProductModel
+    {
+        public ProductModel(int id, string name, string description, string color, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Color = color;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+
+        //[Display(Name = "Name")]
+        //[Required(ErrorMessage = "This field is required")]
+        public string Name { get; set; } = string.Empty;
+
+        //[Display(Name = "Description")]
+        //[Required(ErrorMessage = "This field is required")]
+        public string Description { get; set; } = string.Empty;
+        public string Color { get; set; }
+
+        //[Display(Name = "Price")]
+        //[Required(ErrorMessage = "This field is required")]
+        public decimal Price { get; set; }
+        //public string ErrorM { get; set; } = "";
+    }
+}
