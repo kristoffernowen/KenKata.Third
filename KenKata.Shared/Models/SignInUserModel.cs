8820cc5408edaee11b05111e96b394d5e957ce47
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2022-05-12 16:43:04 +0200

    SignIn and out done as a partial view navbar says I'm in (needs to change)

---
+++ KenKata.Shared/Models/SignInUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace KenKata.Shared.Models
+{
+    public class SignInUserModel
+    {
+        public string UserNameOrEmail { get; set; } = string.Empty;
+
+       
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
