6c0badd9f8d6403eff26db92b7a9c7716746b5d8
jessica <jessicamolin@hotmail.com>
2022-05-13 16:54:46 +0200

    adminProductController Products,create,Edit,Details

--- KenKata.WebApp/Controllers/AdminProductController.cs
+++ KenKata.WebApp/Controllers/AdminProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KenKata.Shared.Models;
+using KenKata.WebApp.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KenKata.WebApp.Controllers
 {
@@ -8,6 +10,101 @@ namespace KenKata.WebApp.Controllers
         {
             return View();
         }
+        private readonly IProductService _productService;
 
+        public AdminProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //[Authorize(Roles = "admin")]
+        [Route("Products")]
+        [HttpGet]
+        public async Task<IActionResult> Products()
+        {
+            var _products = await _productService.GetAll();
+
+            return View(_products);
+        }
+        //[Authorize(Roles = "admin")]
+        [Route("Create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new ProductModelForm();
+            return View(model);
+        }
+
+        ////[Authorize(Roles = "admin")]
+        //[Route("Create")]
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductModelForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                var productResult = await _productService.Create(model);
+
+                if (productResult.Success)
+                {
+                    return RedirectToAction("Products", "AdminProduct");
+                }
+                else
+                {
+                    model.ErrorM = "The name cant be the same as existing name";
+                    return View(model);
+                }
+            }
+            else
+            {
+                model.ErrorM = "Pleace fill in the form";
+                return View(model);
+            }
+        }
+        ////[Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("Edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productService.Get(id);
+
+            var Model = new ProductModel()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price
+            };
+            return View(Model);
+
+
+        }
+        ////[Authorize(Roles = "admin")]
+        ////[ValidateAntiForgeryToken]
+        [Route("edit/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, ProductModelForm model)
+        {
+            var result = await _productService.Update(id, model);
+
+            if (result.Success)
+            {
+                return RedirectToAction("Products", "AdminProduct");
+            }
+            else
+                model.ErrorM = "Pleace fill in all fields";
+            return View(model);
+        }
+        ////[Authorize(Roles = "admin")]
+        //[HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _productService.Get(id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            else
+                return View(NotFound());
+        }
     }
 }
