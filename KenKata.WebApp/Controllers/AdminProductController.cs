0a81dac6eb59721b85d0f08f8cd85842c5310e64
jessica <jessicamolin@hotmail.com>
2022-05-13 16:46:03 +0200

    adminProductController added

---
+++ KenKata.WebApp/Controllers/AdminProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KenKata.WebApp.Controllers
+{
+    public class AdminProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
