c69cd304cd1e1bc9a3abda247044f8086f11f658
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2022-05-24 16:37:57 +0200

    Hans addToCart js added

--- KenKata.WebApp/Controllers/ShoppingCartController.cs
+++ KenKata.WebApp/Controllers/ShoppingCartController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KenKata.Shared.Models;
+using KenKata.WebApp.Service;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace KenKata.WebApp.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        private readonly IProductService _productService;
+
+        public ShoppingCartController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            var shoppingCart = new ShoppingCart();
+            var session = HttpContext.Session.GetString("ShoppingCart");
+
+            if (id != 0)
+            {
+                if (!string.IsNullOrEmpty(session))
+                {
+                    shoppingCart = JsonConvert.DeserializeObject<ShoppingCart>(session);
+                    int index = shoppingCart.Items.FindIndex(x => x.Product.Id == id);
+
+                    if (index != -1)
+                    {
+                        shoppingCart.Items[index].Quantity += 1;
+                    }
+                    else
+                    {
+                        shoppingCart.Items.Add(new CartItem { Product = await _productService.Get(id) });
+                    }
+                }
+                else
+                {
+                    shoppingCart.Items.Add(new CartItem { Product = await _productService.Get(id)});
+                }
+            }
+
+            HttpContext.Session.SetString("ShoppingCart", JsonConvert.SerializeObject(shoppingCart));
+            return new OkObjectResult(HttpContext.Session.GetString("ShoppingCart"));
         }
     }
 }
--- KenKata.WebApp/wwwroot/js/site.js
+++ KenKata.WebApp/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+function addToCart(event) {
+    console.log("hej");
+    
+    event.stopPropagation()
+    fetch(`https://localhost:7167/shoppingcart/addtocart/${event.currentTarget.dataset.product}`)
+        .then(res => res.text())
+        .then(data => {
+            localStorage.setItem("shoppingCart", data)
+            getCart() 
+        })
+        
+}
+
+function getCart() {
+
+    let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"))
+    if (shoppingCart === undefined) {
+        fetch(`https://localhost:7167/shoppingcart/addtocart/0`)
+            .then(res => res.text())
+            .then(data => {
+                localStorage.setItem("shoppingCart", data)
+
+                shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"))
+                document.getElementById('totalQuantity').innerText = shoppingCart.TotalQuantity;
+                document.getElementById('totalPrice').innerText = shoppingCart.TotalPrice;
+            })
+    } else {
+        document.getElementById('totalQuantity').innerText = shoppingCart.TotalQuantity;
+        document.getElementById('totalPrice').innerText = shoppingCart.TotalPrice;
+    }
+}
+
+getCart()
