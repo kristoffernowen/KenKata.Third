1d9909bd0d7f085b6faa8e39feb17d76862089ca
jessica <jessicamolin@hotmail.com>
2022-05-26 16:38:13 +0200

    BlogController added

---
+++ KenKata.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KenKata.WebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
