96a2b6509df5eb0ff27acf9b37ba31131cb67a87
jessica <jessicamolin@hotmail.com>
2022-05-27 15:36:36 +0200

    AdminBlogController Added

---
+++ KenKata.WebApp/Controllers/AdminBlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KenKata.WebApp.Controllers
+{
+    public class AdminBlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
