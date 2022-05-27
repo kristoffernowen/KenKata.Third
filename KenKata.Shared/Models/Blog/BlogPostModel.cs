ddb28311237911c6f4f091a1b71b5d95d98bbc82
jessica <jessicamolin@hotmail.com>
2022-05-27 15:21:47 +0200

    BlogPostModel added TagModel added

---
+++ KenKata.Shared/Models/Blog/BlogPostModel.cs
@@ -0,0 +1,24 @@
+﻿using KenKata.Shared.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenKata.Shared.Models
+{
+    public class BlogPostModel
+    {
+        public int Id { get; set; }
+        public string Rubrik { get; set; }
+        public string ImgUrl { get; set; }
+        public string SecundImgUrl { get; set; }
+        public string ThirdImgUrl { get; set; }
+        public string Text { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public string Author { get; set; }
+        public string category { get; set; }
+        public IEnumerable<TagModel> tags { get; set; }
+    }
+}
---
+++ KenKata.Shared/Models/Blog/TagModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenKata.Shared.Models.Blog
+{
+    public class TagModel
+    {
+        public string Id { get; set; }
+        public string TagName { get; set; }
+    }
+}
