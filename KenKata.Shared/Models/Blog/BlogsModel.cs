9a222dddc6a5c5f7a696f91389a31b79f06c6528
jessica <jessicamolin@hotmail.com>
2022-05-27 15:34:09 +0200

    added models

---
+++ KenKata.Shared/Models/Blog/BlogsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenKata.Shared.Models.Blog
+{
+    public class BlogsModel
+    {
+        //Till blog första sidan
+        public int Id { get; set; }
+        public string Rubrik { get; set; }
+        public string ShortText { get; set; }
+        public string Arthur { get; set; }
+        public DateTime DateTime { get; set; }
+        public IEnumerable<TagModel> Tags { get; set; }
+        public string ImgUrl { get; set; }
+        
+    }
+}
