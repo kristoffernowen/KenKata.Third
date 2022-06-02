7858a2c26436daae640e5061af77c64d2a451d2f
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2022-06-02 11:11:21 +0200

    Team index view created, banner added

---
+++ KenKata.WebApp/Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KenKata.WebApp.Controllers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
