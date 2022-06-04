6736448eb40413085710bc3b0772e1c38285ae45
Evgenija Chernogorova <evgenija555@live.com>
2022-06-04 10:55:45 +0200

    controller

---
+++ KenKata.WebApp/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KenKata.WebApp.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WomensFashion()
+        {
+            return View();
+        }
+    }
+
+
+}
